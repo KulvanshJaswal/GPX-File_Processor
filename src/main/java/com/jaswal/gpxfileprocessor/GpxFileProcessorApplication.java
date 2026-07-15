b35ebc931a25fc40f5ca82fc36232838c324552c
Kulvansh <kulvansh.jaswal@ucalgary.ca>
2026-07-15 15:16:24 -0600

    Initial Spring Boot project skeleton

---
+++ src/main/java/com/jaswal/gpxfileprocessor/GpxFileProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.jaswal.gpxfileprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GpxFileProcessorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GpxFileProcessorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jaswal/gpxfileprocessor/GpxFileProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaswal.gpxfileprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GpxFileProcessorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
